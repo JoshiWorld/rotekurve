baecc3c1f0595fbe6dc2d2c70733771f4f794634
JoshiWorld <joshua.stieber.1903@gmail.com>
2024-04-26 13:35:35 +0200

    updated footer color in dark

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ export function SiteFooter() {
     const currentYear = new Date().getFullYear();
 
   return (
-    <Footer container className="sticky bottom-0 mt-10 rounded-none">
+    <Footer container className="sticky bottom-0 mt-10 rounded-none dark:bg-zinc-800">
       <FooterCopyright href="#" by="Rotekurve" year={currentYear} />
       <FooterLinkGroup>
         <FooterLink href="/impressum">Impressum</FooterLink>
