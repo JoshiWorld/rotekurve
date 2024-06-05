5d2951a7ff3b419c8071bf00a3f942ffb15b3154
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-06-05 12:49:52 +0200

    fixed unter uns

--- src/app/_components/internal/aboutus/load-aboutus.tsx
+++ src/app/_components/internal/aboutus/load-aboutus.tsx
@@ -1,11 +1,12 @@
 import { api } from "@/trpc/server";
 import { type AboutUs } from "@prisma/client";
 import { EditAboutus } from "./edit-aboutus";
+import { CreateAboutus } from "./create-aboutus";
 
 export default async function AboutusEditLoad() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-redundant-type-constituents
   const aboutus: AboutUs | null = await api.aboutus.getLatest();
-  if (!aboutus) return null;
+  if (!aboutus) return <CreateAboutus />;
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   return <EditAboutus aboutus={aboutus} />;
