6bb7076b7ef1cead5b934e4ba5a541c8693aacbd
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-08-02 14:28:17 +0200

    fixed wrong page format

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -5,10 +5,12 @@ export default async function AboutUs() {
   const abouts = await api.aboutus.getLatest();
 
   return (
-    <div
-      className="mt-8 flex flex-col items-center gap-2"
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      dangerouslySetInnerHTML={{ __html: abouts?.content ?? "" }}
-    />
+    <div className="mt-8 flex flex-col items-center gap-2">
+      <div
+        className="w-full max-w-4xl px-6 md:px-10"
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        dangerouslySetInnerHTML={{ __html: abouts?.content ?? "" }}
+      />
+    </div>
   );
 }
